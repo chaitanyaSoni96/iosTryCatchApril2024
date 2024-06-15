e8147376cd6d7aadb8954420bad7ad858925e4d3
Chaitanya Soni <sonichaitanya13@gmail.com>
2024-06-15 14:42:03 +0530

    ui doubts june 15

---
+++ ui doubts/ui doubts/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ui doubts
+//
+//  Created by Chaitanya Soni on 15/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    var tapGesture: UITapGestureRecognizer?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func tap(_ sender: Any) {
+        
+        print("some")
+        
+    }
+    
+
+}
+
